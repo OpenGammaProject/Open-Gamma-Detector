3fc8b312b60861835151989e569ed3ef9e909c2e
Phoenix1747 <apertured@public-files.de>
2021-11-20 20:24:54 +0100

    add new arduino sketch

---
+++ arduino/opensc_pico/opensc_pico.ino
@@ -0,0 +1,86 @@
+/*
+   Scintillation Counter Sketch
+   Adapted to be used with the Raspberry Pi Pico
+   Simple detector sketch returning the pulse heights
+
+   ADC Settings:
+    - Standard is 12-bit, 4096
+*/
+
+#include <PicoAnalogCorrection.h> // Analog Calibration
+
+const uint8_t GND_PIN = A0; // GND meas pin
+const uint8_t VCC_PIN = A2; // VCC meas pin
+const uint8_t VSYS_MEAS = A3; // VSYS/3
+const uint8_t VBUS_MEAS = 24; // VBUS Sense Pin
+
+const uint8_t AIN_PIN = A1; // Analog input pin
+const uint8_t INT_PIN = 13; // Signal interrupt pin
+const uint8_t RST_PIN = 5; // Peak detector MOSFET reset pin
+const uint8_t LED = 25; // LED on GP25
+
+PicoAnalogCorrection pico; // (10,4095)
+
+void event_int() {
+  digitalWrite(LED, HIGH); // Activity LED
+
+  uint16_t m = pico.analogCRead(AIN_PIN,5); // Average 5 corrected measurements
+  
+  digitalWrite(RST_PIN, HIGH); // Reset peak detector
+  delayMicroseconds(1);
+  digitalWrite(RST_PIN, LOW);
+
+  //Serial.println(3.3 / 4095 * m, 3);
+  Serial.println(m);
+  
+  digitalWrite(LED, LOW);
+}
+
+void setup() {
+  pinMode(PS_PIN, OUTPUT);
+  digitalWrite(PS_PIN, LOW); // Enable Power-Saving
+  
+  pinMode(GND_PIN, INPUT);
+  pinMode(VCC_PIN, INPUT);
+  pinMode(VSYS_MEAS, INPUT);
+  pinMode(VBUS_MEAS, INPUT);
+}
+
+void setup1() {
+  pinMode(INT_PIN, INPUT);
+  pinMode(RST_PIN, OUTPUT);
+  pinMode(AIN_PIN, INPUT);
+  pinMode(LED, OUTPUT);
+
+  attachInterrupt(digitalPinToInterrupt(INT_PIN), event_int, HIGH);
+
+  Serial.begin();
+  while(!Serial) {
+    ; // Wait for Serial
+  }
+
+  //pico.calibrateAdc(GND_PIN, VCC_PIN, 5000);
+  //Serial.print("Offset: ");
+  //pico.returnCalibrationValues();
+}
+
+void loop() {
+  /*
+  digitalWrite(PS_PIN, HIGH); // Disable Power Save For Better Noise
+  Serial.print("Temperature: ");
+  Serial.println(analogReadTemp(),1);
+  digitalWrite(PS_PIN, LOW); // Re-Enable Power Saving
+  
+  Serial.print("Supply Voltage: ");
+  Serial.println(3*pico.analogCRead(VSYS_MEAS)*3.3/4095,3);
+  
+  Serial.print("USB Connection: ");
+  Serial.println(digitalRead(VBUS_MEAS));
+  Serial.println();
+  */
+  delay(1000);
+}
+
+void loop1() {
+  // Do not use, because interrupts run on this core!
+}
