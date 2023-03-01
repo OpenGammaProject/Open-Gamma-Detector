2a48d1a2e31a5389517ab97ddda252b7f5504cb7
Phoenix1747 <apertured@public-files.de>
2023-03-01 15:32:38 +0100

    add degree symbol

--- software/opengamma_pico/opengamma_pico.ino
+++ software/opengamma_pico/opengamma_pico.ino
@@ -223,7 +223,7 @@ void deviceInfo([[maybe_unused]] String *args) {
   println("Used Heap Memory: " + String(rp2040.getUsedHeap() / 1000.0) + " kB");
   println("Free Heap Memory: " + String(rp2040.getFreeHeap() / 1000.0) + " kB");
   println("Total Heap Size: " + String(rp2040.getTotalHeap() / 1000.0) + " kB");
-  println("Temperature: " + String(round(readTemp()) * 10.0) / 10.0, 1) + " °C");
+  println("Temperature: " + String(round(readTemp() * 10.0) / 10.0, 1) + " °C");
   println("USB Connection: " + String(digitalRead(VBUS_MEAS)));
 
   const float v = 3.0 * analogRead(VSYS_MEAS) * VREF_VOLTAGE / (pow(2, ADC_RES) - 1);
@@ -466,12 +466,14 @@ void drawSpectrum() {
   const int16_t temp = round(readTemp());
 
   if (temp < 0) {
-    display.setCursor(SCREEN_WIDTH - 30, 0);
+    display.setCursor(SCREEN_WIDTH - 36, 0);
   } else {
-    display.setCursor(SCREEN_WIDTH - 24, 0);
+    display.setCursor(SCREEN_WIDTH - 30, 0);
   }
   display.print(temp);
-  display.println(" °C");
+  display.print(" ");
+  display.print((char)247);
+  display.println("C");
 
   const uint32_t seconds_running = round(time_delta / 1000.0);
   const uint8_t char_offset = floor(log10(seconds_running));
